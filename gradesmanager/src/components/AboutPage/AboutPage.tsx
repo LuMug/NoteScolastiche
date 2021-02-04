de1f3f550ad5a1b68ca1f9f01950ffe3556295fb
Nicola Ambrosetti <61270869+nicolaambrosetti@users.noreply.github.com>
2021-02-04 15:33:57 +0100

    Create AboutPage.tsx

---
+++ gradesmanager/src/components/AboutPage/AboutPage.tsx
@@ -0,0 +1,15 @@
+import './AboutPage.css';
+
+
+function AboutPage() {
+    return (
+        <div className="main-content">
+            <h1 className="title">ABOUT</h1>
+            <p className="text">Questo progetto è stato svolto sul arco del secondo semstre del anno scolastico 2020-2021.
+                <br />In esso sono stati convolti gli allievi: Ambrosetti Nicola, Previtali Aris, Trentin Ismael e Viola Francisco.
+            </p>
+        </div>
+    );
+}
+
+export default AboutPage;
