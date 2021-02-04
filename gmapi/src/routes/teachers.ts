b7d54c2f9b2e906f843d9302d74fae9d25061ee3
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-02-04 08:52:14 +0100

    Create teachers route

---
+++ gmapi/src/routes/teachers.ts
@@ -0,0 +1,106 @@
+import express, { Router, Request, Response } from 'express';
+import { IError, ITeacher } from '../@types';
+import { MongoHelper } from '../helpers/MongoHelper';
+
+const router: Router = express.Router();
+
+router.get('/teachers', async (req: Request, res: Response) => {
+    let arr = await MongoHelper.asArray(MongoHelper.getTeachers());
+    res.status(200).json(arr);
+});
+
+router.get('/teachers/:uid', async (req: Request, res: Response) => {
+    let uid: number = parseInt(req.params.uid);
+    if (isNaN(uid)) {
+        return res.status(400).json({
+            error: {
+                message: 'Not a valid teacher id.'
+            }
+        });
+    }
+    let teacher: ITeacher | null = await MongoHelper.getTeacher(uid);
+    if (teacher) {
+        return res.status(201).json(teacher);
+    } else {
+        res.status(400).json({
+            error: {
+                message: `No teacher with id: ${uid}`
+            }
+        });
+    }
+});
+
+router.post('/teachers', async (req: Request, res: Response) => {
+    if (!MongoHelper.isTeacher(req.body.teacher)) {
+        let err: IError = {
+            message: 'Bad JSON.'
+        };
+        return res.status(400).json({ error: err });
+    }
+    let teacher: ITeacher = req.body.teacher as ITeacher;
+    try {
+        await MongoHelper.addTeacher(teacher);
+    } catch (err) {
+        return res.status(409).json(err);
+    }
+    res.status(201).json(teacher);
+});
+
+router.post('/teachers/:uid/subjectsIds', async (req: Request, res: Response) => {
+    // 1 numero, [...] numeri
+    let uid: number = parseInt(req.params.uid);
+    if (isNaN(uid)) {
+        let err: IError = {
+            message: 'Not a valid teacher id.'
+        };
+        return res.status(400).json({ error: err });
+    }
+    let input: any = req.body.subjectsIds;
+    if (Array.isArray(input)) {
+        // check if all are numbers...
+        try {
+            await MongoHelper.addSubjectId(uid, ...input);
+        } catch (err) {
+            return res.status(400).json({ error: { message: err } });
+        }
+    } else if (!isNaN(parseInt(input))) {
+        try {
+            await MongoHelper.addSubjectId(uid, input);
+        } catch (err) {
+            return res.status(400).json({ error: { message: err } });
+        }
+    } else {
+        let err: IError = {
+            message: 'Invalid input. Must be an array of positive numbers.'
+        }
+        return res.status(400).json({ error: err });
+    }
+    return res.status(204).json();
+});
+
+router.patch('/teachers/:uid', async (req: Request, res: Response) => {
+    let uid: number = parseInt(req.params.uid);
+    let teacher: Partial<Omit<ITeacher, 'uid'>> = req.body.teacher;
+    if (isNaN(uid)) {
+        let err: IError = {
+            message: 'Invalid uid.'
+        }
+        return res.status(400).json(err);
+    }
+    try {
+        await MongoHelper.updateTeacher(uid, teacher);
+    } catch (err) {
+        let error: IError;
+        if (typeof err == 'string') {
+            error = {
+                message: err
+            };
+        } else {
+            error = err;
+        }
+        return res.status(400).json(error);
+    }
+    return res.status(204).json();
+});
+
+export default router;
