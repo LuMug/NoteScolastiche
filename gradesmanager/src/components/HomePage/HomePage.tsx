68b05f1afd9f5bce41a01aa039a6cd7ee3d2bce2
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-02-11 15:38:44 +0100

    Create HomePage component

--- gradesmanager/src/components/HomePage/HomePage.tsx
+++ gradesmanager/src/components/HomePage/HomePage.tsx
@@ -1,147 +1,107 @@
-import React, { Component, ReactNode } from 'react';
+import { Component, ReactNode } from 'react';
 import { IUser, IUserSubject } from '../../@types';
 import Subject from '../subject/Subject';
 import './home-page.css'
+import { API_URL } from './../../util/constants';
+import LoadingPage from '../LoadingPage/LoadingPage';
+import { Router, withRouter } from 'react-router-dom';
+import AddSubject from '../add-subject/AddSubject';
 
-interface IHomePageProps {
+interface IHomePageState {
 
-    user: IUser;
+    loading: boolean;
+
+    user: IUser | null;
 }
-interface State { }
 
-class HomePage extends Component<IHomePageProps, State> {
+class HomePage extends Component<{}, IHomePageState> {
 
-    constructor(props: IHomePageProps) {
+    constructor(props: {}) {
         super(props);
+        this.state = {
+            loading: true,
+            user: null
+        };
+    }
+
+    async componentDidMount() {
+        const url: string = `${API_URL}users/0`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({
+            loading: false,
+            user: data
+        });
     }
 
     render(): ReactNode {
+        if (this.state.loading || this.state.user == null) {
+            return <LoadingPage />;
+        }
         return (
-            <div className="main-content">
-                <div className="side-panel">
-                    <div className="side-panel-section">
-                        <p className="route-el">Home</p>
-                        <p className="route-el">Materie</p>
-                        <p className="route-el">Route X</p>
+            <div className="hp-main-content">
+                <div className="hp-side-panel">
+                    <div className="hp-side-panel-section">
+                        <p className="hp-route-el">Home</p>
+                        <p className="hp-route-el">Materie</p>
+                        <p className="hp-route-el">Route X</p>
                     </div>
-                    <div className="side-panel-separator"></div>
-                    <div id="subjects-list-panel" className="side-panel-section">
-                        <p className="side-panel-section-title">Materie</p>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
-                        <div className="subject-wrap">
-                            <p className="subject-el"></p>
-                            <div className="mock-text"></div>
-                        </div>
+                    <div className="hp-side-panel-separator"></div>
+                    <div id="hp-subjects-list-panel" className="hp-side-panel-section">
+                        <p className="hp-side-panel-section-title">Materie</p>
+                        {this.state.user.subjects.map((us: IUserSubject) => {
+                            return <div className="hp-subject-wrap">
+                                <p className="hp-subject-el">{us.name}</p>
+                            </div>
+                        })}
                     </div>
                 </div>
-                <div className="content-page">
-                    <div className="welcome-panel">
-                        <h1 className="welcome-text">Benvenuto, <span>{this.props.user.name}</span></h1>
+                <div className="hp-content-page">
+                    <div className="hp-welcome-panel">
+                        <h1 className="hp-welcome-text">Benvenuto, <span>{this.state.user.name}</span></h1>
                     </div>
-                    <div className="welcome-separator"></div>
-                    <div className="card trend-panel rise-opacity-in">
-                        <div className="chart-wrapper">
-                            <div className="chart">
-                                <canvas id="chartCanvas"></canvas>
+                    <div className="hp-welcome-separator"></div>
+                    <div className="hp-card trend-panel hp-rise-opacity-in">
+                        <div className="hp-chart-wrapper">
+                            <div className="hp-chart">
+                                <canvas id="hp-chartCanvas"></canvas>
                             </div>
                         </div>
-                        <div className="trend-data">
-                            <div id="section1" className="trend-data-column">
-                                <div className="trend-data-column-title">Section 1</div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
+                        <div className="hp-trend-data">
+                            <div id="hp-section1" className="hp-trend-data-column">
+                                <div className="hp-trend-data-column-title">Section 1</div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
                             </div>
-                            <div className="trend-data-separator"></div>
-                            <div id="section2" className="trend-data-column">
-                                <div className="trend-data-column-title">Section 2</div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
-                                <div className="trend-data-mock"></div>
+                            <div className="hp-trend-data-separator"></div>
+                            <div id="hp-section2" className="hp-trend-data-column">
+                                <div className="hp-trend-data-column-title">Section 2</div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
+                                <div className="hp-trend-data-mock"></div>
                             </div>
                         </div>
-                        <div className="trend-data-page-switcher">
-                            <div id="left"></div>
-                            <div id="right"></div>
+                        <div className="hp-trend-data-page-switcher">
+                            <div id="hp-left"></div>
+                            <div id="hp-right"></div>
                         </div>
                     </div>
-                    <div className="subjects">
-                        {this.props.user.subjects.map((s: IUserSubject) => {
-                            return <Subject name="dsad" subjectObj={s} teacherName="teacher name" />
+                    <div className="hp-subjects">
+                        {this.state.user.subjects.map((s: IUserSubject) => {
+                            return <Subject subject={s} />
                         })}
+                        <AddSubject />
                     </div>
                 </div>
             </div>
@@ -149,4 +109,6 @@ class HomePage extends Component<IHomePageProps, State> {
     }
 }
 
-export default HomePage
+
+
+export default HomePage;
