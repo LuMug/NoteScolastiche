d5141d81202dafd098a198aafe52f17c0b57d280
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-02-11 15:38:33 +0100

    Create AddSubject component

---
+++ gradesmanager/src/components/add-subject/AddSubject.tsx
@@ -0,0 +1,12 @@
+interface Props { }
+
+function AddSubject(props: Props) {
+
+    return (
+        <div className="as-main">
+            <div className="as-icon">+</div>
+        </div>
+    );
+}
+
+export default AddSubject;
