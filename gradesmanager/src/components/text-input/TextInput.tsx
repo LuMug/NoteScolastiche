43e30e9ff5f31a3cf0154f471eff2e830b29c7a0
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-02-25 15:34:53 +0100

    Add number type property

--- gradesmanager/src/components/text-input/TextInput.tsx
+++ gradesmanager/src/components/text-input/TextInput.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './TextInput.css';
 
 export interface ITextInputProps {
-    inputType: 'text' | 'password';
+    inputType: 'text' | 'password' | 'number';
     placeHolder: string;
     toolTipText: string;
 }
@@ -14,16 +14,23 @@ export default class TextInput extends Component<ITextInputProps> {
     }
 
     render() {
+        let input;
+        if (this.props.inputType == 'number') {
+            input = <input
+                type={this.props.inputType}
+                step={0.25}
+                autoComplete="off"
+                spellCheck="false"
+                title={this.props.toolTipText}
+                tabIndex={1}
+                pattern=""
+                min={1}
+                max={6}
+            />;
+        }
         return (
             <div className="ti-input">
-                <input
-                    type={this.props.inputType}
-                    autoComplete="off"
-                    spellCheck="false"
-                    title={this.props.toolTipText}
-                    tabIndex={1}
-                    pattern=""
-                />
+                {input}
                 <label className="ti-label">
                     <span>{this.props.placeHolder}</span>
                 </label>
