7192612ac450fe90ae0bb64cb3a5f0c9d0704682
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-02-25 09:18:04 +0100

    Add message when unavailable

--- gradesmanager/src/components/LoadingPage/LoadingPage.tsx
+++ gradesmanager/src/components/LoadingPage/LoadingPage.tsx
@@ -1,16 +1,30 @@
-import React, { Component, ReactNode } from 'react'
+import React, { Component, ReactNode } from 'react';
 import './loading-page.css';
 
-class LoadingPage extends Component {
+interface ILoadingPageProps {
+
+    unavailable?: boolean;
+}
+
+class LoadingPage extends Component<ILoadingPageProps> {
 
     constructor(props: {}) {
         super(props);
     }
 
     render(): ReactNode {
+        let cName = (this.props.unavailable)
+            ? 'lp-msg-container'
+            : 'lp-hidden';
         return (
             <div className="lp-main">
-                <div className="lp-loading-bar"></div>
+                <div className="lp-loading-bar">
+                    <div className={cName}>
+                        <p className="lp-msg">Oh no!</p>
+                        <p className="lp-msg-hint">Siamo offline, prova a ricaricare la pagina tra un po'</p>
+                    </div>
+
+                </div>
             </div>
         );
     }
