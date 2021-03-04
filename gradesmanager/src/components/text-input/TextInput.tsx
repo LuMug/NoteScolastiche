11b87170320e671244e4d2237d6b004464bda5bf
Ismael Trentin <ismael.trentin@samtrevano.ch>
2021-03-04 17:56:36 +0100

    Fix input type prop

--- gradesmanager/src/components/text-input/TextInput.tsx
+++ gradesmanager/src/components/text-input/TextInput.tsx
@@ -2,39 +2,28 @@ import React, { Component } from 'react';
 import './TextInput.css';
 
 export interface ITextInputProps {
+
     inputType: 'text' | 'password' | 'number';
+
     placeHolder: string;
+
     toolTipText: string;
 }
 
-export default class TextInput extends Component<ITextInputProps> {
-
-    constructor(props: ITextInputProps) {
-        super(props);
-    }
-
-    render() {
-        let input;
-        if (this.props.inputType == 'number') {
-            input = <input
-                type={this.props.inputType}
-                step={0.25}
-                autoComplete="off"
-                spellCheck="false"
-                title={this.props.toolTipText}
-                tabIndex={1}
-                pattern=""
-                min={1}
-                max={6}
-            />;
-        }
-        return (
-            <div className="ti-input">
-                {input}
-                <label className="ti-label">
-                    <span>{this.props.placeHolder}</span>
-                </label>
-            </div>
-        );
-    }
+export default function TextInput(props: ITextInputProps) {
+    let input = <input
+        type={props.inputType}
+        autoComplete="off"
+        spellCheck="false"
+        title={props.toolTipText}
+        pattern=""
+    />
+    return (
+        <div className="ti-input">
+            {input}
+            <label className="ti-label">
+                <span>{props.placeHolder}</span>
+            </label>
+        </div>
+    );
 }
