154c7186ce2c5496218b1fa7bdd8ddf28fe45cb1
Nicola Ambrosetti <61270869+nicolaambrosetti@users.noreply.github.com>
2021-03-04 15:26:17 +0100

    Add Teachers test route

--- gradesmanager/src/App.tsx
+++ gradesmanager/src/App.tsx
@@ -1,11 +1,12 @@
-import AboutButton from './components/about-button/AboutButton';
-import AboutPage from './components/AboutPage/AboutPage';
-import GradePrompt from './components/grade-prompt/GradePrompt';
-import HomePage from './components/HomePage/HomePage';
-import LoginPage from './components/LoginPage/LoginPage';
-import SubjectPage from './components/SubjectPage/SubjectPage';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import { Route, Router } from 'react-router-dom';
+import AboutButton from "./components/about-button/AboutButton";
+import AboutPage from "./components/AboutPage/AboutPage";
+import GradePrompt from "./components/grade-prompt/GradePrompt";
+import HomePage from "./components/HomePage/HomePage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import SubjectPage from "./components/SubjectPage/SubjectPage";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { Route, Router } from "react-router-dom";
+import TeacherPage from "./components/TeacherPage/TeacherPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/teachers" component={TeacherPage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/about" component={AboutPage} />
           <Route exact path="/test" component={GradePrompt} />
