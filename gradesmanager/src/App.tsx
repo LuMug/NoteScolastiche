789baab79af3ac63474b910e1e528145a0428c9e
Ismael Trentin <ismael.trentin@samtrevano.ch>
2021-03-04 17:52:29 +0100

    Add test route

--- gradesmanager/src/App.tsx
+++ gradesmanager/src/App.tsx
@@ -1,21 +1,19 @@
-import AboutButton from './components/about-button/AboutButton';
 import AboutPage from './components/AboutPage/AboutPage';
-import GradePrompt from './components/grade-prompt/GradePrompt';
 import HomePage from './components/HomePage/HomePage';
 import LoginPage from './components/LoginPage/LoginPage';
-import SubjectPage from './components/SubjectPage/SubjectPage';
+import TeacherInfobox from './components/teacher-infobox/TeacherInfobox';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import { Route, Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="root">
+    <div>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/test" component={GradePrompt} />
+          <Route exact path="/test" component={TeacherInfobox} />
         </Switch>
         {/* <Route exact path="/subjects/:suid" component={SubjectPage} /> */}
       </BrowserRouter>
