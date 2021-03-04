ce06a14e401f1869f1de061305382e3c48083b26
Ismael Trentin <ismael.trentin@samtrevano.ch>
2021-03-04 17:57:09 +0100

    Add routes

--- gradesmanager/src/util/constants.ts
+++ gradesmanager/src/util/constants.ts
@@ -1,1 +1,18 @@
+import { IRouteDescritor } from '../@types';
+
 export const API_URL: string = 'http://127.0.0.1:5000/api/v1/';
+
+export const ROUTES: IRouteDescritor[] = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'About',
+        path: '/about'
+    },
+    {
+        name: 'Logout',
+        path: '/logout'
+    },
+];
