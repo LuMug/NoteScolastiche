e081dd4d1829a833019106b972cdb6aa9452a8d4
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-03-11 08:47:59 +0100

    Add logic

--- gradesmanager/src/components/SubjectPage/SubjectPage.tsx
+++ gradesmanager/src/components/SubjectPage/SubjectPage.tsx
@@ -1,122 +1,145 @@
-import React, { Component, ReactNode } from 'react';
-import { API_URL } from './../../util/constants';
+import AddGradeButton from '../add-grade-btn/AddGradeButton';
+import FetchHelper from '../../helpers/FetchHelper';
+import GradeHelper from '../../helpers/GradeHelper';
+import GradeOptions from '../grade-options/GradeOptions';
+import Subject from '../subject/Subject';
+import { Component, ReactNode } from 'react';
 import { IGrade, ITeacher, IUserSubject } from '../../@types';
-import { RouteComponentProps } from 'react-router-dom';
 import './subject-page.css';
 
-type SubjectPageProps = {
-    uuid: number,
-    suid: number
-};
+interface ISubjectPageProps {
 
-//interface ISubjectPageProps extends RouteComponentProps<SubjectPageProps> { }
+    subject: IUserSubject;
+
+    onAbort: () => void;
+
+    onAddGrade: () => void;
+
+    onEditGrade?: (grade: IGrade, index: number) => void;
+
+    onRemoveGrade: (grade: IGrade, index: number) => void;
+}
 
 interface ISubjectPageState {
 
     loading: boolean;
-    subject: IUserSubject | null;
-    teacher: ITeacher | null;
-    prevState?: ISubjectPageState;
+
+    subject: IUserSubject;
+
+    teacher?: ITeacher;
+
+    teacherFullname?: string;
 }
 
-class SubjectPage extends Component<SubjectPageProps, ISubjectPageState> {
+class SubjectPage extends Component<ISubjectPageProps, ISubjectPageState> {
 
-    constructor(props: SubjectPageProps) {
+    constructor(props: ISubjectPageProps) {
         super(props);
         this.state = {
             loading: true,
-            subject: null,
-            teacher: null
+            subject: props.subject
         };
     }
 
     async componentDidMount() {
-        let uuid = this.props.uuid;//parseInt(this.props.uuid);
-        let suid = this.props.suid;//parseInt(this.props.suid);
-        //suid = 1;
-        if (isNaN(uuid)) {
-            console.error('Invalid uuid for subject detail');
-            return;
-        } else if (isNaN(suid)) {
-            console.error('Invalid suid for subject detail');
+        this.setUp();
+    }
+
+    componentDidUpdate(prevProps: ISubjectPageProps, prevState: ISubjectPageState) {
+        if (this.props != prevProps) {
+            this.setUp();
+        }
+    }
+
+    private async setUp() {
+        if (this.props.subject.teacherId) {
+            let teacher: ITeacher;
+            try {
+                teacher = await FetchHelper.fetchTeacher(this.props.subject.teacherId);
+            } catch (err) {
+                console.error(err);
+                return;
+            }
+            this.setState({
+                loading: false,
+                teacher: teacher,
+                teacherFullname: `${teacher.surname} ${teacher.name}`,
+                subject: this.props.subject
+            });
             return;
         }
-        let url = `${API_URL}users/${uuid}/subjects/${suid}`;
-        let res = await fetch(url);
-        const subject: IUserSubject = await res.json();
-        url = `${API_URL}teachers/${subject.teacherId}`;
-        res = await fetch(url);
-        const teacher: ITeacher = await res.json();
         this.setState({
             loading: false,
-            subject: subject,
-            teacher: teacher,
-            prevState: this.state
+            teacherFullname: this.props.subject.teacherName,
+            subject: this.props.subject
         });
     }
 
     render(): ReactNode {
-        if (this.state.loading || !this.state.subject || !this.state.teacher) {
+        if (this.state.loading || !this.state.subject || (!this.state.teacher && !this.state.teacherFullname)) {
             return <h1>loading</h1>
         }
         let grades = this.state.subject.grades;
+        let avg = Subject.getSubjectAvg(this.state.subject);
         let testPlural = (grades.length > 1) ? 's' : '';
-        let avg: number = 0;
-        grades.forEach(g => avg += g.value);
-        avg /= Math.max(grades.length, 1);
         return (
-            <div className="sp-page">
-                <div className="main-content">
-                    <div className="top">
-                        <div className="top-top">
-                            <div className="avg-wrapper">
-                                <div className="avg-border">
-                                    <div className="avg">{(grades.length == 0) ? '' : avg.toFixed(1)}</div>
-                                </div>
-                            </div>
-                            <div className="subject-details">
-                                <div className="details-header">{this.state.subject.name}</div>
-                                <div className="details-teacher">{`${this.state.teacher.surname} ${this.state.teacher.name}`}</div>
-                                <div className="details-data">
-                                    <div className="details-tests">
-                                        <p><span>{grades.length}</span> test{testPlural}</p>
-                                    </div>
-                                    <div className="details-avg">
-                                        <p>Avg: <span>{(grades.length == 0) ? '-' : avg.toFixed(2)}</span></p>
-                                    </div>
-                                </div>
+            <div className="sp-main-content">
+                <div className="sp-abort noselect" onClick={() => this.props.onAbort()}></div>
+                <div className="sp-top">
+                    <div className="sp-top-top">
+                        <div className="sp-avg-wrapper">
+                            <div className="sp-avg-border">
+                                <div className="sp-avg">{(grades.length == 0) ? '' : avg.toFixed(1)}</div>
                             </div>
                         </div>
-                        <div className="top-bottom">
-                            <div className="add-btn"></div>
-                            <div className="options-btn">•••</div>
-                        </div>
-                    </div>
-                    <div className="bottom">
-                        <div className="searchbar">Filtra</div>
-                        <div className="grades-table">
-                            <table>
-                                <tr>
-                                    <th>Grade</th>
-                                    <th>Date</th>
-                                    <th>Weight</th>
-                                    <th>Options</th>
-                                </tr>
-                                {grades.map((g, i) => {
-                                    return (
-                                        <tr key={i}>
-                                            <td className="grade">{g.value}</td>
-                                            <td>{g.date}</td>
-                                            <td>{g.weight}</td>
-                                            <td className="options-icon"></td>
-                                        </tr>
-                                    );
-                                })}
-                            </table>
+                        <div className="sp-subject-details">
+                            <div className="sp-details-header">{this.state.subject.name}</div>
+                            <div className="sp-details-teacher">{this.state.teacherFullname}</div>
+                            <div className="sp-details-data">
+                                <div className="sp-details-tests">
+                                    <p><span>{grades.length}</span> test{testPlural}</p>
+                                </div>
+                                <div className="sp-details-avg">
+                                    <p>Avg: <span className={(grades.length > 0) ? 'sp-details-avg-number' : ''}>{(grades.length == 0) ? '-' : avg.toFixed(2)}</span></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="sp-bottom">
+                    <div className="sp-grades-table">
+                        <table>
+                            <tr className="sp-thead">
+                                <th className="sp-th"><p>Grade</p></th>
+                                <th className="sp-th"><p>Date</p></th>
+                                <th className="sp-th"><p>Weight</p></th>
+                                <th className="sp-th"><p>Options</p></th>
+                            </tr>
+                            {this.state.subject.grades.map((g, i) => {
+                                return <tr className="sp-tr" key={i}>
+                                    <td className="sp-td sp-grade">{GradeHelper.valueToString(g)}</td>
+                                    <td className="sp-td">{GradeHelper.getDate(g)}</td>
+                                    <td className="sp-td">{g.weight.toFixed(1)}</td>
+                                    <td className="sp-td sp-options">
+                                        <div className="sp-options-wrapper">
+                                            <GradeOptions
+                                                onOptionClick={(oi) => {
+                                                    if (oi == 1) {
+                                                        this.props.onRemoveGrade(g, i);
+                                                    }
+                                                }}
+                                            />
+                                        </div>
+                                    </td>
+                                </tr>;
+                            })}
+                        </table>
+                    </div>
+                    <div className="sp-add-grade-wrapper">
+                        <AddGradeButton onClick={() => this.props.onAddGrade()} />
+                    </div>
+                </div>
+            </div >
         );
     }
 }
