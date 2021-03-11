cacbda739b8f1a28cb6e341cdc842444277cfc21
Nicola Ambrosetti <61270869+nicolaambrosetti@users.noreply.github.com>
2021-03-11 15:11:09 +0100

    Update TeacherPage

--- gradesmanager/src/components/TeacherPage/TeacherPage.tsx
+++ gradesmanager/src/components/TeacherPage/TeacherPage.tsx
@@ -1,58 +1,122 @@
+import { equal } from "assert";
 import "./TeacherPage.css";
 
 function test() {
+  let tdClick = (o: string) => {
+    let el = document.getElementById(o);
+    if (el) {
+      let display = el.style.display;
+      if (el && display == "none") {
+        el.style.display = "block";
+        el.style.visibility = "visible";
+      } else {
+        el.style.display = "none";
+        el.style.visibility = "hidden";
+      }
+    }
+  };
+
   return (
     <div>
-      <div className="tp-buttons-wrapper">
-        <input
-          type="submit"
-          className="tp-class-button"
-          tabIndex={3}
-          value="Classi"
-        />
-        <input
-          type="submit"
-          className="tp-subjects-button"
-          tabIndex={3}
-          value="Materie"
-        />
-      </div>
-      <br></br>
+      <div className="tp-items">
+        <div className="tp-wrapper">
+          <div className="tp-buttons-wrapper">
+            <input
+              type="submit"
+              className="tp-class-button"
+              tabIndex={3}
+              value="Classi"
+              onClick={() => {
+                let el = document.getElementById("tp-class-table");
+                if (el) {
+                  let display = el.style.display;
+                  if (el && display == "none") {
+                    el.style.display = "block";
+                    el.style.visibility = "visible";
+                  } else {
+                    el.style.display = "none";
+                    el.style.visibility = "hidden";
+                  }
+                }
+              }}
+            />
+            <input
+              type="submit"
+              className="tp-subjects-button"
+              tabIndex={3}
+              value="Materie"
+              onClick={() => {
+                let el = document.getElementById("tp-subjects-table");
+                if (el) {
+                  let display = el.style.display;
+                  if (el && display == "none") {
+                    el.style.display = "block";
+                    el.style.visibility = "visible";
+                  } else {
+                    el.style.display = "none";
+                    el.style.visibility = "hidden";
+                  }
+                }
+              }}
+            />
+          </div>
+          <br></br>
 
-      <div>
-        <table className="tp-subjects-table">
-          <tr>
-            <td>Modulo 122</td>
-          </tr>
-          <tr>
-            <td>Modulo 123</td>
-          </tr>
-          <tr>
-            <td>Modulo 141</td>
-          </tr>
-        </table>
-      </div>
+          <div>
+            <table className="tp-subjects-table" id="tp-subjects-table">
+              <tr>
+                <td onClick={() => tdClick("tp-class-table")}>Modulo 122</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-class-table")}>Modulo 123</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-class-table")}>Modulo 141</td>
+              </tr>
+            </table>
+          </div>
 
-      <br></br>
+          <br></br>
 
-      <div>
-        <table className="tp-class-table">
-          <tr>
-            <td>I1A</td>
-          </tr>
-          <tr>
-            <td>I1B</td>
-          </tr>
-          <tr>
-            <td>I2A</td>
-          </tr>
-          <tr>
-            <td>I2B</td>
-          </tr>
-          <tr>
-            <td>I3A</td>
-          </tr>
-        </table>
+          <div>
+            <table className="tp-class-table" id="tp-class-table">
+              <tr>
+                <td onClick={() => tdClick("tp-students-table")}>I1A</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-students-table")}>I1B</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-students-table")}>I2A</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-students-table")}>I2B</td>
+              </tr>
+              <tr>
+                <td onClick={() => tdClick("tp-students-table")}>I3A</td>
+              </tr>
+            </table>
+          </div>
+
+          <br></br>
+
+          <div>
+            <table className="tp-students-table" id="tp-students-table">
+              <tr>
+                <td>Nicola Ambrosetti</td>
+              </tr>
+              <tr>
+                <td>Francisco Viola</td>
+              </tr>
+              <tr>
+                <td>Aris Previtali</td>
+              </tr>
+              <tr>
+                <td>Ismael Trentin</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
