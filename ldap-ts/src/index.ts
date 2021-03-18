c589c24f13568bcf0541cc71b246ec8d23bdd560
ArisPrevitali <aris.previtali@samtrevano.ch>
2021-03-18 15:36:11 +0100

    Add index.ts for publish npm package

--- ldap-ts/src/index.ts
+++ ldap-ts/src/index.ts
@@ -1,17 +1,3 @@
-import { LDAPClient } from "./LDAPClient";
-
-let ldap = new LDAPClient();
-let nome: string = "nicola.ambrosetti";
-let password: string = "";
-
-const main = async () => {
-  
-  await ldap.start();
-  let check = await ldap.checkUserCredentials(nome, password);
-  
-  console.log(check);
-  
-  ldap.end();
-};
-
-main();
+export * from './ADUser';
+export * from './LDAPClient';
+export * from './PathParser';
