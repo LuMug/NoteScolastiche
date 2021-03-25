a2cb6cd9c2ecd4b4c28e42704c839b4c2480b1f1
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-03-25 12:45:33 +0100

    Create Page model component

---
+++ gradesmanager/src/components/Page/Page.tsx
@@ -0,0 +1,60 @@
+import FetchHelper from '../../helpers/FetchHelper';
+import Nav from '../nav/Nav';
+import React, { Component, ReactNode } from 'react';
+import { IUser, IUserSubject } from '../../@types';
+import { ROUTES } from './../../util/constants';
+import './page.css';
+
+interface IPageProps {
+
+    user: IUser | null;
+
+    content: JSX.Element;
+
+    displayPrompt: boolean;
+
+    promptElement?: JSX.Element;
+
+    onListSubjectClick?: (us: IUserSubject, index: number) => void;
+}
+
+interface IPageState {
+
+    user: IUser | null;
+}
+
+class Page extends Component<IPageProps, IPageState> {
+
+    constructor(props: IPageProps) {
+        super(props);
+        this.state = {
+            user: props.user
+        };
+    }
+
+    render(): ReactNode {
+        let prompt =
+            <div className={`pa-prompt ${(this.props.displayPrompt) ? '' : 'hidden'}`}>
+                {this.props.promptElement}
+            </div>;
+        return (
+            <div className="pa-main-content">
+                <Nav
+                    routes={ROUTES}
+                    entries={(this.state.user) ? this.state.user.subjects.map(s => s.name) : []}
+                    onEntryClick={(i) => {
+                        if (this.props.onListSubjectClick && this.state.user) {
+                            this.props.onListSubjectClick(this.state.user.subjects[i], i);
+                        }
+                    }}
+                />
+                <div className="pa-content-page">
+                    {this.props.content}
+                </div>
+                {prompt}
+            </div>
+        );
+    }
+}
+
+export default Page;
