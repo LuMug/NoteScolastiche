ab8bffb4b2b149ad3011ef5dcd6445e947311067
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-03-25 12:41:30 +0100

    Add authors route and update home route

--- gradesmanager/src/App.tsx
+++ gradesmanager/src/App.tsx
@@ -1,7 +1,7 @@
 import AboutPage from './components/AboutPage/AboutPage';
+import AuthorsPage from './components/AuthorsPage/AuthorsPage';
 import HomePage from './components/HomePage/HomePage';
 import LoginPage from './components/LoginPage/LoginPage';
-import TeacherInfobox from './components/teacher-infobox/TeacherInfobox';
 import TeacherPage from './components/TeacherPage/TeacherPage';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
@@ -11,13 +11,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={HomePage} />
           <Route exact path="/teachers" component={TeacherPage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/test" component={TeacherInfobox} />
+          <Route exact path="/authors" component={AuthorsPage} />
+          <Route path="/:uuid" render={(props) => <HomePage uuid={props.match.params.uuid} />} />
         </Switch>
-        {/* <Route exact path="/subjects/:suid" component={SubjectPage} /> */}
       </BrowserRouter>
     </div>
   );
