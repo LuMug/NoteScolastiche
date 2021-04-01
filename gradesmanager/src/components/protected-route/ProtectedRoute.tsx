eaabcc03649bf18dd767c6024126e77d07e63f80
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-04-01 09:30:43 +0200

    Create ProtectedRoute component

---
+++ gradesmanager/src/components/protected-route/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import Auth from '../../auth/Auth';
+import React, { Component } from 'react';
+import { Redirect, Route, RouteProps } from 'react-router';
+
+interface IProtectedRouteProps extends RouteProps { }
+
+function ProtectedRoute(props: IProtectedRouteProps) {
+    if (!Auth.isLoggedIn()) {
+        // return <Redirect to='/login' />
+    }
+    return (
+        <Route {...props} exact={props.exact} />
+    );
+}
+
+export default ProtectedRoute;
