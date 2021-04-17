683612d26bb35dc54a210a4f584cdc45925ba1ee
Ismael Trentin <ismael.trentin@samtrevano.ch>
2021-04-17 04:49:22 +0200

    Update Slider

--- gradesmanager/src/components/slider/Slider.tsx
+++ gradesmanager/src/components/slider/Slider.tsx
@@ -3,24 +3,25 @@ import './slider.css';
 
 interface ISliderProps {
 
-  onChangeState: (on: boolean) => void;
+  default?: boolean;
+
+  onChangeState?: (on: boolean) => void;
 }
 
-class Slider extends Component<ISliderProps> {
-  constructor(props: ISliderProps) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <div className="slider-main-content">
-        <label className="form-switch">
-          <input type="checkbox" onChange={e => this.props.onChangeState((e.target.value == 'true') ? true : false)} />
-          <i></i>
-        </label>
-      </div>
-    );
-  }
+const Slider: React.FunctionComponent<ISliderProps> = (props) => {
+  let def = (props.default) ? true : false;
+  return (
+    <div className="slider-main-content">
+      <label className="form-switch">
+        <input type="checkbox" defaultChecked={def} onChange={e => {
+          if (props.onChangeState) {
+            props.onChangeState(e.target.checked);
+          }
+        }} />
+        <i></i>
+      </label>
+    </div>
+  );
 }
 
 export default Slider;
