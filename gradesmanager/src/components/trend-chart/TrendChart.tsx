02828d11414bc5d44342643b436513df342586a1
Ismael Trentin <ismael.trentin@samtrevano.ch>
2021-04-17 04:51:27 +0200

    Update to use AbstractChart

--- gradesmanager/src/components/trend-chart/TrendChart.tsx
+++ gradesmanager/src/components/trend-chart/TrendChart.tsx
@@ -1,84 +1,35 @@
 import * as ObjectHelper from './../../helpers/ObjectHelper';
+import AbstractChart, { IAbstractChartProps } from '../chart/AbstractChart';
 import React, { Component, ReactNode } from 'react';
 import { Chart } from 'chart.js';
 
-export type TrendChartDataSet = {
-
-    label: string;
-
-    data: number[];
-
-    backgroundColor?: string | CanvasGradient;
-};
-
-interface ITrendChartProps {
-
-    labels: string[];
-
-    dataset: TrendChartDataSet;
-}
-
-
-class TrendChart extends Component<ITrendChartProps> {
-
-    private chartRef: any;
-
-    private chart: Chart | null;
-
-    constructor(props: ITrendChartProps) {
-        super(props);
-        this.chartRef = React.createRef();
-        this.chart = null;
-    }
-
-    componentDidUpdate(prevProps: ITrendChartProps) {
-
-
-        //if (!ObjectHelper.equals(prevProps, this.props)) {
-        this.chart = this.buildChart();
-        this.chart.update();
-        //}
-    }
-
-    componentDidMount() {
-        this.chart = this.buildChart();
-    }
-
-    private buildChart() {
-        let ctx: CanvasRenderingContext2D = this.chartRef.current.getContext('2d');
-        let gradient = ctx.createLinearGradient(0, 0, 0, 310);
-        gradient.addColorStop(0.3, '#007eff');
-        gradient.addColorStop(1, '#007eff00');
-        this.props.dataset.backgroundColor = gradient;
-        return new Chart(this.chartRef.current, {
-            type: 'line',
-            data: {
-                labels: this.props.labels,
-                datasets: [this.props.dataset]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: {
-                                min: 1,
-                                max: 6
-                            }
+const TrendChart: React.FunctionComponent<IAbstractChartProps> = (props) => {
+    return <AbstractChart
+        dataset={props.dataset}
+        labels={props.labels}
+        type="line"
+        options={{
+            responsive: true,
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            min: 1,
+                            max: 6
                         }
-                    ],
-                    unitStepSize: 0.25
-                },
-                maintainAspectRatio: false,
+                    }
+                ],
+                unitStepSize: 0.25
+            },
+            maintainAspectRatio: false,
+        }}
+        shouldUpdate={props.shouldUpdate}
+        onUpdate={() => {
+            if (props.onUpdate) {
+                props.onUpdate();
             }
-        });
-    }
-
-    render(): ReactNode {
-        return (
-            <canvas ref={this.chartRef} />
-        );
-    }
+        }}
+    />
 }
 
 export default TrendChart;
