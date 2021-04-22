203edde169fa297eca4f04558aa6db7cc6f6b76f
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-04-22 09:00:55 +0200

    Fix authorizing when both tokens are missing

--- gmapi/src/middlewares/auth.ts
+++ gmapi/src/middlewares/auth.ts
@@ -1,17 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { NextFunction, Request, Response } from 'express';
 
 dotenv.config({
     path: path.join(__dirname, '..', '.env')
 });
 
 export default (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header("authorization");
-    // console.log(process.env.AUTH_TOKEN);
-
-    if (token == process.env.AUTH_TOKEN) {
-        (req as any).authorized = true;
+    const token = req.header("Authorization");
+    if (token) {
+        if (token === process.env.AUTH_TOKEN) {
+            (req as any).authorized = true;
+        }
     } else {
         (req as any).authorized = false;
     }
