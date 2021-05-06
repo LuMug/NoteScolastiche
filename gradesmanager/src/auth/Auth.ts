064b49a939b38097a89c0282af55771437fce143
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-05-06 13:49:11 +0200

    Fix login

--- gradesmanager/src/auth/Auth.ts
+++ gradesmanager/src/auth/Auth.ts
@@ -1,39 +1,39 @@
 import FetchHelper from '../helpers/FetchHelper';
-import { IUser } from '../@types';
+import internals from './internals.json';
+import { IError, IUser, UserType } from '../@types';
 
 class Auth {
 
-    public async login(username: string, password: string): Promise<number | null> {
-        let ok = false;
-        let uid: number | null = null;
-        let body = {
-            username: username,
-            password: password
-        };
+    public async login(username: string, password: string) {
+        let ok: IUser | null = null;
         try {
-            ok = await FetchHelper.fetch('/authentication', 'POST', body);
-            // Fetches anyway so that we can have standalone accounts only on our db.
-            uid = await FetchHelper.fetchUserUid(username);
+            ok = await FetchHelper.login(username, password);
         } catch (err) {
-            console.error(err);
+            let internal: IUser | null = await this.getInternalAccount(username, password);
+            if (internal) {
+                sessionStorage.setItem('logged', 'true');
+                sessionStorage.setItem('uid', internal.uid.toString());
+                sessionStorage.setItem('user_type', internal.type);
+                return internal;
+            }
+            throw err;
         }
-        if (!ok && uid) {
-            // internal users
-            ok = password === '@Admin123@';
-        }
-        if (ok && uid) {
+        if (ok && ok.uid) {
             sessionStorage.setItem('logged', 'true');
-            sessionStorage.setItem('uid', uid.toString());
+            sessionStorage.setItem('uid', ok.uid.toString());
+            sessionStorage.setItem('user_type', ok.type);
+            return ok;
         } else {
             sessionStorage.setItem('logged', 'false');
             sessionStorage.removeItem('uid');
             return null;
         }
-        return uid;
     }
 
     public logout() {
-        sessionStorage.setItem('logged', 'false');
+        sessionStorage.removeItem('logged');
+        sessionStorage.removeItem('uid');
+        sessionStorage.removeItem('user_type');
     }
 
     public isLoggedIn(): boolean {
@@ -43,6 +43,42 @@ class Auth {
     public getUserUid(): number | null {
         let uid = sessionStorage.getItem('uid');
         return (uid) ? parseInt(uid) : null;
+    }
+
+    public getUserType(): UserType | null {
+        let entry = sessionStorage.getItem('user_type');
+        return (entry) ? entry as UserType : null;
+    }
+
+    public setUserType(type: UserType) {
+        sessionStorage.setItem('user_type', type.toString());
+    }
+
+    // public getUserTheme(): 'blue_theme' | 'purple_theme' | null {
+    //     return sessionStorage.getItem('theme') as 'blue_theme' | 'purple_theme' | null;
+    // }
+
+    private async getInternalAccount(username: string, password: string) {
+        for (const acc of internals) {
+            if (acc.username === username && acc.password === password) {
+                let uid: number | null = null;
+                try {
+                    uid = await FetchHelper.fetchUserUid(username);
+                    let user: IUser | null = null;
+                    if (uid) {
+                        user = await FetchHelper.fetchUser(uid);
+                    }
+                    if (!uid || !user) {
+                        return null;
+                    }
+                    return user;
+                } catch (err) {
+                    console.error(err);
+                    return null;
+                }
+            }
+        }
+        return null;
     }
 }
 
