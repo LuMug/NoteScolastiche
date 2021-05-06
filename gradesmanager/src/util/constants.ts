596e1d775784eb2ee4e31705f59670fb0005cda7
IsmaelTrentin <61277905+IsmaelTrentin@users.noreply.github.com>
2021-05-06 14:15:46 +0200

    Update consts

--- gradesmanager/src/util/constants.ts
+++ gradesmanager/src/util/constants.ts
@@ -1,22 +1,28 @@
 import { IRouteDescritor } from '../@types';
 
-export const API_URL: string = 'http://127.0.0.1:5000/api/v1/';
+export const API_URL = 'http://127.0.0.1:5000/api/v1/';
+
+export const HOME_ROUTE = '/';
+
+export const ADMIN_ROUTE = '/admins';
+
+export const TEACHERS_ROUTE = '/teachers';
+
+export const ABOUT_ROUTE = '/about';
+
+export const LOGIN_ROUTE = '/login';
 
 export const ROUTES: IRouteDescritor[] = [
     {
         name: 'Home',
-        path: '/'
+        path: HOME_ROUTE
     },
     {
-        name: 'Autori',
-        path: '/authors'
-    },
-    {
-        name: 'test',
-        path: '/admins'
+        name: 'Informazioni',
+        path: ABOUT_ROUTE
     },
     {
         name: 'Logout',
-        path: '/login'
+        path: LOGIN_ROUTE
     }
 ];
